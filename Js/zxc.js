6f2b66f9d623b970f2c6f0ec742562790d7bb635
paluszkiewicz <bubusowaty@gmail.com>
2019-02-06 01:44:31 +0100

    zadanie

--- Js/zxc.js
+++ Js/zxc.js
@@ -6,7 +6,18 @@ alert("pole: " + pole);
 
 
 
+Pole(promien);
+
+let a = parseFloat(prompt("Podaj promień koła"));
+let h = parseFloat(prompt("Podaj promień koła"));
+
+function ObliczPoleTrojkata(a,h){
+  return ((a*h)/2);
+}
+
 function PoleTrojkata(){
   let pole = ObliczPoleTrojkata(a,h);
   document.write("Pole trójkąta o boku równym: " + a + "cm<sup>2</sup> i wysokości równej: " + h + "cm<sup>2</sup> równa się: " + pole + "cm<sup>2</sup>");
 }
+
+PoleTrojkata(a,h);
