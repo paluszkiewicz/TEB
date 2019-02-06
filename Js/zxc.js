0261763228466eebfe7dbfece495b8b8b12d0b88
paluszkiewicz <bubusowaty@gmail.com>
2019-02-06 01:40:50 +0100

    Merge branch 'master' of https://github.com/paluszkiewicz/TEB

--- Js/zxc.js
+++ Js/zxc.js
@@ -1,3 +1,12 @@
 let promien = prompt("podaj promień");
 let pole = Math.PI * Math.pow(promien, 2);
 alert("pole: " + pole);
+
+
+
+
+
+function PoleTrojkata(){
+  let pole = ObliczPoleTrojkata(a,h);
+  document.write("Pole trójkąta o boku równym: " + a + "cm<sup>2</sup> i wysokości równej: " + h + "cm<sup>2</sup> równa się: " + pole + "cm<sup>2</sup>");
+}
